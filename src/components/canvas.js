e00c6622daaec7231e712918da7e9269a2889e25
Apekshit <apekshit33@gmail.com>
2023-06-18 17:55:07 +0530

    Add functionality to add images in the canvas

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -9,7 +9,8 @@ const Canvas = () => {
   const [startY, setStartY] = useState(0);
   const [endX, setEndX] = useState(0);
   const [endY, setEndY] = useState(0);
-  const [shapes, setShapes] = useState([]);
+  const [layers, setLayers] = useState([]);
+  const [images, setImages] = useState([]);
 
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
@@ -28,6 +29,22 @@ const Canvas = () => {
     context.lineWidth = 5;
     contextRef.current = context;
   }, []);
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = async () => {
+    try {
+      const response = await fetch(
+        "https://api.slingacademy.com/v1/sample-data/photos"
+      );
+      const data = await response.json();
+      setImages(data.photos);
+    } catch (error) {
+      console.log("Error fetching images:", error);
+    }
+  };
 
   const startDrawing = ({ nativeEvent }) => {
     const { offsetX, offsetY } = nativeEvent;
@@ -51,35 +68,22 @@ const Canvas = () => {
       endX,
       endY,
     };
-    setShapes((prevShapes) => [...prevShapes, shape]);
+    setLayers((prevLayers) => [...prevLayers, shape]);
   };
 
-  const drawShapes = () => {
+  const drawLayers = () => {
     contextRef.current.clearRect(
       0,
       0,
       canvasRef.current.width,
       canvasRef.current.height
     );
-
-    shapes.forEach((shape) => {
-      const { type, startX, startY, endX, endY } = shape;
-      if (type === "circle") {
-        const radius = Math.sqrt((endX - startX) ** 2 + (endY - startY) ** 2);
-        contextRef.current.beginPath();
-        contextRef.current.arc(startX, startY, radius, 0, 2 * Math.PI);
-        contextRef.current.stroke();
-      } else if (type === "rectangle") {
-        contextRef.current.beginPath();
-        contextRef.current.rect(startX, startY, endX - startX, endY - startY);
-        contextRef.current.stroke();
-      } else if (type === "triangle") {
-        contextRef.current.beginPath();
-        contextRef.current.moveTo(startX, startY);
-        contextRef.current.lineTo(startX, endY);
-        contextRef.current.lineTo(endX, endY);
-        contextRef.current.closePath();
-        contextRef.current.stroke();
+    layers.forEach((layer) => {
+      if (layer.type === "image") {
+        const { image, x, y, width, height } = layer;
+        contextRef.current.drawImage(image, x, y, width, height);
+      } else {
+        drawShape(layer);
       }
     });
 
@@ -124,17 +128,33 @@ const Canvas = () => {
 
     setEndX(offsetX);
     setEndY(offsetY);
-    drawShapes();
+    drawLayers();
   };
 
   const selectShape = (shape) => {
     setSelectedShape(shape);
-    // setShapes([]);
+    // setLayers([]);
+  };
+
+  const loadSidebarImage = (imageUrl) => {
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = () => {
+      const layer = {
+        type: "image",
+        image: img,
+        x: 0,
+        y: 0,
+        width: canvasRef.current.width,
+        height: canvasRef.current.height,
+      };
+      setLayers((prevLayers) => [...prevLayers, layer]);
+    };
   };
 
   useEffect(() => {
-    drawShapes();
-  }, [shapes]);
+    drawLayers();
+  }, [layers]);
 
   return (
     <Fragment>
@@ -146,6 +166,16 @@ const Canvas = () => {
           <button onClick={() => selectShape("triangle")}>Triangle</button>
         </div>
         <h2>Images</h2>
+        <div className="images">
+          {images.map((image) => (
+            <img
+              key={image.id}
+              src={image.url}
+              alt={image.title}
+              onClick={() => loadSidebarImage(image.url)}
+            />
+          ))}
+        </div>
       </div>
       <div className="draw-area">
         <canvas
